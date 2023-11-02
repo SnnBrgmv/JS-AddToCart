155dbeced9dcef30568bb9bdf8f455582325001c
SnnBrgmv <sananai@code.edu.az>
2023-11-03 02:06:46 +0400

    Complete JS Add-To-Cart

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 export const products = [
-    {id: 1, name: "Orange", price: 1.22, imgSrc: "orange.jpg"},
-    {id: 2, name: "Banana", price: 3.0, imgSrc: "banana.jpg"},
-    {id: 3, name: "Lemon", price: .5, imgSrc: "lemon.jpg"}
+    { id: 1, name: "Orange", price: 1.22, imgSrc: "orange.jpg" },
+    { id: 2, name: "Banana", price: 3.0, imgSrc: "banana.jpg" },
+    { id: 3, name: "Lemon", price: .5, imgSrc: "lemon.jpg" }
 ];
--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-// Öncelikle, products adında bir ürün listesi oluşturun (data.js dosyasından alabilirsiniz).
 import { products } from "./data.js";
 
 const productContainer = document.querySelector(".product-container");
 const cartContainer = document.querySelector(".cart-container");
 const totalPrice = document.querySelector(".total-price");
-let cart = []; // Alışveriş sepeti verilerini burada saklayacağız
+let cart = [];
 
-// Ürünleri ana sayfada görüntüleme
+// Create product cards for each product
 products.forEach((product) => {
     let { id, name, price, imgSrc } = product;
     let productCard = generateProductCard(id, name, price, imgSrc);
     productContainer.innerHTML += productCard;
 });
 
-// Yardımcı işlev: Ürün kartları oluştur
+// Function to generate a product card HTML
 function generateProductCard(id, name, price, imgSrc) {
     let productCard = `
         <div class="col">
@@ -21,7 +20,7 @@ function generateProductCard(id, name, price, imgSrc) {
                 <img src="./images/${imgSrc}" class="card-img-top" alt="${name}">
                 <div class="card-body">
                     <h5 class="card-title">${name}</h5>
-                    <p class="card-text">Price: $${price}</p>
+                    <p class="card-text fs-6">Price: $${price}</p>
                     <button data-id=${id} class="btn btn-primary add-to-cart-btn">Add to cart</button>
                 </div>
             </div>
@@ -29,14 +28,29 @@ function generateProductCard(id, name, price, imgSrc) {
     return productCard;
 }
 
-// Tüm "Add to cart" düğmelerini alın ve sepete eklemek için işlevsellik ekleyin
+// Load the cart from localStorage when the page is loaded
+window.addEventListener('load', () => {
+    const storedCart = JSON.parse(localStorage.getItem('cart'));
+    if (storedCart) {
+        cart = storedCart;
+        renderCart();
+        updateCartCount();
+    }
+});
+
+// Function to update the cart in localStorage
+function updateLocalStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+// Add-to-cart button click event listeners
 document.querySelectorAll(".add-to-cart-btn").forEach((btn) => {
     btn.onclick = (e) => {
         let id = e.target.getAttribute("data-id");
         let product = products.find((p) => p.id == id);
 
         if (product) {
-            // Ürünü sepete ekleyin veya adetini artırın
+
             let cartItem = cart.find((item) => item.id == id);
             if (cartItem) {
                 cartItem.quantity++;
@@ -44,43 +58,43 @@ document.querySelectorAll(".add-to-cart-btn").forEach((btn) => {
                 cart.push({ id, product, quantity: 1 });
             }
 
-            // Sepeti güncelleyin
             renderCart();
             updateCartCount();
+            updateLocalStorage();
         }
     };
 });
 
-// Alışveriş sepetini görüntülemek için işlev
+// Function to render the cart
 function renderCart() {
-    // Sepeti temizleyin
+
     cartContainer.innerHTML = "";
     let total = 0;
 
-    // Sepetteki ürünleri görüntüle
+
     cart.forEach((cartItem) => {
         let { id, product, quantity } = cartItem;
         let cartItemHTML = `
-        <div class="col d-flex gap-3">
-            <p>${product.name}</p>
-            <span>(${product.price})</span>
-            <div class="d-flex">
+        <div class="col w-100 d-flex gap-3 justify-content-between">
+            <div class="product-name d-flex justify-content-center align-items-center fs-6"><span>${product.name}</span></div>
+            <div class="product-price d-flex justify-content-center align-items-center"><span>(${product.price})</span></div>
+            <div class="product-quantity d-flex justify-content-center align-items-center">
                 <button class="btn btn-light cart-btn decrease-btn" data-id=${id}>-</button>
                 <div class="cart-count">${quantity}</div>
                 <button class="btn btn-light cart-btn increase-btn" data-id=${id}>+</button>
             </div>
-            <span class="delete-cart-item"><i class="bi bi-x delete-btn" data-id=${id}></i></span>
-            <span>$${product.price * quantity}</span>
+            <div class="product-delete d-flex justify-content-center align-items-center"><span class="delete-cart-item p-3"><i class="bi bi-x delete-btn" data-id=${id}></i></span></div>
+            <div class="product-total d-flex justify-content-center align-items-center"><span>$${(product.price * quantity).toFixed(2)}</span></div>
         </div>`;
         cartContainer.innerHTML += cartItemHTML;
 
         total += product.price * quantity;
     });
 
-    // Toplam fiyatı güncelleyin
-    totalPrice.innerHTML = "Total price: $" + total;
+    totalPrice.innerHTML = `Total price: <span class="fw-normal">$${total.toFixed(2)}</span>`;
 }
 
+// Click event listener for the cart items
 cartContainer.addEventListener('click', (e) => {
     const target = e.target;
     if (target.classList.contains('increase-btn')) {
@@ -90,6 +104,7 @@ cartContainer.addEventListener('click', (e) => {
             cartItem.quantity++;
             renderCart();
         }
+        updateCartCount();
     } else if (target.classList.contains('decrease-btn')) {
         const id = target.getAttribute('data-id');
         const cartItem = cart.find((item) => item.id == id);
@@ -97,26 +112,28 @@ cartContainer.addEventListener('click', (e) => {
             cartItem.quantity--;
             renderCart();
         }
+        updateCartCount();
     } else if (target.classList.contains('delete-btn')) {
         const id = target.getAttribute('data-id');
         cart = cart.filter((item) => item.id != id);
         renderCart();
+        updateCartCount();
     }
 });
+
+// Function to update the cart count
 function updateCartCount() {
     const cartCountElement = document.getElementById("cart-count");
     const count = cart.reduce((total, item) => total + item.quantity, 0);
     cartCountElement.textContent = count;
 }
 
-// Add event listener for the "Clear Cart" button
+// Click event listener for clearing the cart
 document.getElementById("clear-cart-btn").addEventListener("click", () => {
-    // Clear the cart by resetting the cart array
     cart = [];
-    
-    // Update the cart container and total price
     renderCart();
     updateCartCount();
+    updateLocalStorage();
 });
 
 
